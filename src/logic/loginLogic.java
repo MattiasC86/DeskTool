19208326f185d1d0633fa05912e5176515e306cd
sebastian-johansson <sebastian_johansson@hotmail.se>
2017-05-09 14:47:00 +0200

    login logic

--- src/logic/loginLogic.java
+++ src/logic/loginLogic.java
@@ -1,4 +1,27 @@
 package logic;
 
+
+import service.UserService;
+
+
 public class loginLogic {
+
+    UserService user;
+    int userLevel;
+    String dataUsername, datapassword;
+
+    public int loginValidation(String username, String password){
+
+        user.readByName(dataUsername);
+
+        if (username == dataUsername){
+            System.out.println("username ok but not password");
+        }
+
+        else {
+            System.out.println("do not work");
+        }
+
+        return(userLevel);
+    }
 }
