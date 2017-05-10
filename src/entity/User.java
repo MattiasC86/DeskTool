4d7a15940893bdc7a6a467dc0cbc887bc5bf36c4
sebastian-johansson <sebastian_johansson@hotmail.se>
2017-05-10 10:50:36 +0200

    User
    setters and getters for email and username

--- src/entity/User.java
+++ src/entity/User.java
@@ -60,6 +60,14 @@ public class User {
         this.password = password;
     }
 
+    public String getUserName() {return userName; }
+
+    public void setUserName(String userName) {this.userName = userName; }
+
+    public String getEmail () { return email; }
+
+    public void setEmail (String email) {this.email = email;}
+
     public String getRole() {
         return role;
     }
