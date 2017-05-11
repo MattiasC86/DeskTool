215cdb82e7b372227043fb5047270a6c0ad86f12
sebastian-johansson <sebastian_johansson@hotmail.se>
2017-05-11 14:22:49 +0200

    loginlogic
    added user logic

--- src/logic/loginLogic.java
+++ src/logic/loginLogic.java
@@ -1,34 +1,67 @@
 package logic;
-
 import service.UserService;
+import logic.UserLogic;
 
 
 public class LoginLogic {
 
     UserService user;
+    UserLogic currentUser;
     String dataUsername, dataPassword, userLevel;
-    boolean access = false;
+    String curUserFirstName, curUserlastName,  curUseruserName,  curUserpassword, curUseremail, curUserrole ;
+    public static int access = 1; //0 = false, 1 = true, 2 = username empty, 3 = password empty
 
     public String loginValidation(String formUsername, String formPassword){
 
         dataUsername = user.read("userName", formUsername, "userId");
         dataPassword = user.read("password", formPassword, "userId");
 
-        if (dataUsername.equals(dataPassword)){
-            access = true;
-            userLevel = user.read("userName", formUsername, "role");
+        curUserFirstName = user.read("userName", formUsername, "firstName");
+        curUserlastName = user.read("userName", formUsername, "lastName");
+        curUseruserName = user.read("userName", formUsername, "userName");
+        curUserpassword = user.read("userName", formUsername, "password");
+        curUseremail = user.read("userName", formUsername, "email");
+        curUserrole = user.read("userName", formUsername, "role");
+/*
+        if (dataUsername != null && !dataUsername.isEmpty()){
+            access = 2;
         }
 
         else {
-            access = false;
+            if (dataPassword != null && !dataPassword.isEmpty()){
+                access = 3;
+            }
+
+            else {
+                if (dataUsername.equals(dataPassword)){
+                    access = 1;
+                    userLevel = user.read("userName", formUsername, "role");
+                    //currentUser = new UserLogic(curUserFirstName, curUserlastName, curUseruserName, curUserpassword, curUseremail, curUserrole);
+                }
+
+                else {
+                    access = 0;
+                    System.out.println("Wrong username or password!");
+                }
+            }
+        }*/
+
+        if (dataUsername.equals(dataPassword)){
+            access = 1;
+            userLevel = user.read("userName", formUsername, "role");
+            //currentUser = new UserLogic(curUserFirstName, curUserlastName, curUseruserName, curUserpassword, curUseremail, curUserrole);
+        }
+
+        else {
+            access = 0;
             System.out.println("Wrong username or password!");
         }
+
 
         return(userLevel);
     }
 
-    public boolean Accesslevel(){
-
+    public int Accesslevel(){
         return access;
     }
 }
