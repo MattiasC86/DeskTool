9b9b62ac89f559274a2bc5aa67366ca11dde43be
MattiasC86 <froggehxl@gmail.com>
2017-05-11 10:30:49 +0200

    Kommenterat kod i registerLogic

--- src/logic/registerLogic.java
+++ src/logic/registerLogic.java
@@ -1,3 +1,7 @@
+/*
+* This class handles validation logic when a new user is to be created.
+*/
+
 package logic;
 
 
@@ -10,15 +14,17 @@ public class registerLogic {
     public static int registerUser(String firstName, String lastName, String userName, String email, String password, String role) {
         String result1 = UserService.read("userName", userName, "userName");
         String result2 = UserService.read("email", email, "email");
+
+        //If username already exists in db - return 1
         if(result1.equals(userName)) {
             return 1;
         }
+        //If email already exists in db - return 2
         if(result2.equals(email)) {
             return 2;
         }
+        //If username and email is not taken, save new user in db - return 3
         UserService.create(new User(firstName, lastName, userName, email, password, role));
         return 3;
     }
-
-
 }
