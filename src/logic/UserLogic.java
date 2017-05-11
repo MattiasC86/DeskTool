6a08c1a0e0eb3d6d918db287c7223b4273762552
sebastian-johansson <sebastian_johansson@hotmail.se>
2017-05-11 14:19:56 +0200

    userlogic

--- src/logic/UserLogic.java
+++ src/logic/UserLogic.java
@@ -4,8 +4,60 @@ import service.UserService;
 
 public class UserLogic {
 
-    public void UserLogic()
-    {
+    private String firstName;
+    private String lastName;
+    private String userName;
+    private String password;
+    private String email;
+    private String role;
 
+    public void UserLogic(String firstName, String lastName, String userName, String password, String email, String role)
+    {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.userName = userName;
+        this.password = password;
+        this.email = email;
+        this.role = role;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUserName() {return userName; }
+
+    public void setUserName(String userName) {this.userName = userName; }
+
+    public String getEmail () { return email; }
+
+    public void setEmail (String email) {this.email = email;}
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
     }
 }
