7e3b0005d38c75284db0b8fc18d071254fa652e2
MattiasC86 <froggehxl@gmail.com>
2017-05-17 09:33:03 +0200

    TestAccess - ny entitet
    Ny entitet TestAccess skapad. Håller koll på vilka test som är delade
    med vilka elever.

---
+++ src/entity/TestAccess.java
@@ -0,0 +1,51 @@
+package entity;
+
+
+import javax.persistence.*;
+
+@Entity
+public class TestAccess {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int testAccessId;
+
+    @OneToOne
+    private
+    User user;
+
+    @OneToOne
+    private
+    Test test;
+
+    public TestAccess(User user, Test test) {
+        this.setUser(user);
+        this.setTest(test);
+    }
+
+    public TestAccess(){}
+
+    public int getTestAccessId() {
+        return testAccessId;
+    }
+
+    public void setTestAccessId(int testAccessId) {
+        this.testAccessId = testAccessId;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Test getTest() {
+        return test;
+    }
+
+    public void setTest(Test test) {
+        this.test = test;
+    }
+}
