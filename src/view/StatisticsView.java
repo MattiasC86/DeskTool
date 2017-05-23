37f59c8f42c40905d775b3f06cd0ea9c07e0dd56
MattiasC86 <froggehxl@gmail.com>
2017-05-23 14:44:31 +0200

    Statistiken funkar nu helt
    Men behöver snyggas upp

--- src/view/StatisticsView.java
+++ src/view/StatisticsView.java
@@ -36,6 +36,19 @@ public class StatisticsView extends Application{
     private ComboBox userTestBox;
     FlowPane pane = new FlowPane();
 
+    Label test;
+    Label nrDone;
+    Label nrPassed;
+    Label avgScore;
+    Label avgTime;
+
+    Label status;
+    Label student;
+    Label uTest;
+    Label grade;
+    Label points;
+    Label uTime;
+
     /*
     - Välj elev
         - Välj test
@@ -80,7 +93,7 @@ public class StatisticsView extends Application{
         pane.getChildren().addAll(userBox, testBox);
 
 
-        Scene scene = new Scene(pane, 800, 800);
+        Scene scene = new Scene(pane, 1600, 900);
         primaryStage.setScene(scene);
         primaryStage.show();
 
@@ -114,8 +127,9 @@ public class StatisticsView extends Application{
                 FXCollections.observableArrayList(
                         testNames
                 );
+        pane.getChildren().removeAll(userTestBox, test, nrDone, nrPassed, avgScore, avgTime, status, student, uTest, grade, points, uTime);
         userTestBox = new ComboBox(availableTests);
-        pane.getChildren().remove(userTestBox);
+
         pane.getChildren().addAll(userTestBox);
 
         // When a test is selected for the user selected in userBox
@@ -126,33 +140,38 @@ public class StatisticsView extends Application{
 
     // Shows statistics for selected test
     public void showTestStatistics() {
+        pane.getChildren().removeAll(userTestBox, test, nrDone, nrPassed, avgScore, avgTime, status, student, uTest, grade, points, uTime);
+
         // Loads selected test from db
         int selectedTestIndex = testBox.getSelectionModel().getSelectedIndex();
         Test selectedTest = testList.get(selectedTestIndex);
         System.out.println("TEST ID: " + selectedTest.getTestId());
 
-        Label test  = new Label("Prov: " + selectedTest.gettTitle());
-        Label nrDone = new Label("Antal genomförda: " + StatisticsLogic.getNrDone(selectedTest.getTestId()) +
+        test  = new Label("Prov: " + selectedTest.gettTitle());
+        nrDone = new Label("Antal genomförda: " + StatisticsLogic.getNrDone(selectedTest.getTestId()) +
                 " / " + StatisticsLogic.getNrAccess(selectedTest.getTestId()));
         pane.getChildren().addAll(test, nrDone);
 
         // If any student has done the test
         if(StatisticsLogic.getNrDone(selectedTest.getTestId()) != 0) {
-            Label nrPassed = new Label("Antal godkända: " + StatisticsLogic.getNrPassed(selectedTest.getTestId()));
-            Label avgScore = new Label("Snittpoäng: " + StatisticsLogic.getAvgScore(selectedTest.getTestId()) + " / "
+
+            nrPassed = new Label("Antal godkända: " + StatisticsLogic.getNrPassed(selectedTest.getTestId()));
+            avgScore = new Label("Snittpoäng: " + StatisticsLogic.getAvgScore(selectedTest.getTestId()) + " / "
                     + selectedTest.gettMaxPoints());
             int avgTimeSec = StatisticsLogic.getAvgTime(selectedTest.getTestId());
             int avgTimeMin = avgTimeSec / 60;
             int leftOverSec = avgTimeSec % 60;
-            Label time = new Label("Snittid: " + avgTimeMin + " min " +
+            avgTime = new Label("Snittid: " + avgTimeMin + " min " +
                     leftOverSec + " sek / " + selectedTest.gettTimeMin() + " min");
 
-            pane.getChildren().addAll(nrPassed, avgScore, time);
+            pane.getChildren().addAll(nrPassed, avgScore, avgTime);
         }
     }
 
     // Shows statistics for selected user and test
     public void showUserTestStatistics(User selectedUser) {
+        pane.getChildren().removeAll(test, nrDone, nrPassed, avgScore, avgTime, status, student, uTest, grade, points, uTime);
+
         // Loads selected test from db
         int selectedTestIndex = userTestBox.getSelectionModel().getSelectedIndex();
         Test selectedTest = userTests.get(selectedTestIndex);
@@ -164,16 +183,16 @@ public class StatisticsView extends Application{
 
         // If student havn't yet completed the test
         if(at == null) {
-            Label status = new Label(selectedUser.getFirstName() + " " + selectedUser.getLastName() + " har ännu inte genomfört detta test");
+            status = new Label(selectedUser.getFirstName() + " " + selectedUser.getLastName() + " har ännu inte genomfört detta test");
             pane.getChildren().addAll(status);
 
         //If student has completed the test
         } else {
-            Label student = new Label("Elev: " + selectedUser.getFirstName() + " " + selectedUser.getLastName());
-            Label uTest  = new Label("Prov: " + selectedTest.gettTitle());
-            Label grade = new Label("Betyg: " + at.getaTGrade());
-            Label points = new Label("Poäng:" + at.getaTPoints());
-            Label uTime = new Label("Tidåtgång: " + (at.getaTTimeSec() / 60) + " minuter");
+            student = new Label("Elev: " + selectedUser.getFirstName() + " " + selectedUser.getLastName());
+            uTest  = new Label("Prov: " + selectedTest.gettTitle());
+            grade = new Label("Betyg: " + at.getaTGrade());
+            points = new Label("Poäng:" + at.getaTPoints());
+            uTime = new Label("Tidåtgång: " + (at.getaTTimeSec() / 60) + " minuter");
 
             pane.getChildren().addAll(student, uTest, grade, points, uTime);
         }
