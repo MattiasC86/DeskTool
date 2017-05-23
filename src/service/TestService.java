2953eea707f284e14653c0a1a83401f6670cea5a
MattiasC86 <froggehxl@gmail.com>
2017-05-23 09:51:54 +0200

    Uppdaterat TestService
    Lagt till så att det sparas lite fler frågor och svar i databasen

--- src/service/TestService.java
+++ src/service/TestService.java
@@ -93,18 +93,24 @@ public class TestService {
 
         Question question1 = new Question("Hur många bor det i skogen?", 1, "Single", 0, "G", test1);
         Question question2 = new Question("Vad är en björn?", 1, "Multiple", 1, "G", test1);
-        Question question3 = new Question("Vad är en Newton?", 1, "Single", 0, "G", test2);
+        Question question3 = new Question("Vad är Newton?", 1, "Single", 0, "G", test2);
+        Question question4 = new Question("Bästa elev på Newton?", 1, "Ranked", 1, "G", test2);
         ArrayList<Question> questions = new ArrayList<Question>();
         questions.add(question1);
         questions.add(question2);
         questions.add(question3);
+        questions.add(question4);
 
         Answer answer1 = new Answer("20 personer", 1, 0, question1);
-        Answer answer4 = new Answer("10 personer", 1, 0, question1);
+        Answer answer4 = new Answer("10 personer", 0, 1, question1);
         Answer answer5 = new Answer("Ett djur", 1, 0, question2);
-        Answer answer6 = new Answer("En stad", 1, 0, question2);
+        Answer answer6 = new Answer("En stad", 0, 1, question2);
+        Answer answer7 = new Answer("Något som rör sig", 1, 2, question2);
         Answer answer2 = new Answer("En yrkeshögskola", 1, 0, question3);
         Answer answer3 = new Answer("En förskola", 0, 1, question3);
+        Answer answer8 = new Answer("Mattias", 0, 0, question4);
+        Answer answer9 = new Answer("Övrigt", 0, 2, question4);
+        Answer answer10 = new Answer("Okänd elev", 0, 1, question4);
         ArrayList<Answer> answers = new ArrayList<Answer>();
         answers.add(answer1);
         answers.add(answer2);
@@ -112,7 +118,10 @@ public class TestService {
         answers.add(answer4);
         answers.add(answer5);
         answers.add(answer6);
-
+        answers.add(answer7);
+        answers.add(answer8);
+        answers.add(answer9);
+        answers.add(answer10);
 
         UserService.create(user1);
         UserService.create(user2);
@@ -124,6 +133,6 @@ public class TestService {
         TestAccessService.create(user1, test2);
         TestAccessService.create(user2, test2);
 
-        AnsweredTestService.create(new AnsweredTest(false, false, 5, 400, "G", user1, test2));
+        //AnsweredTestService.create(new AnsweredTest(false, false, 5, 400, "G", user1, test2));
     }
 }
