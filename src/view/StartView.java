9e94654c36ea45683c0a059d3e6028ee1f941ada
MattiasC86 <froggehxl@gmail.com>
2017-06-08 10:02:19 +0200

    Bort med CREATED BY RASMUS

--- src/view/StartView.java
+++ src/view/StartView.java
@@ -3,10 +3,6 @@ package view;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
-/**
- * Created by Rasmus on 2017-05-03.
- */
-
 public class StartView extends Application{
     Stage window;
 
